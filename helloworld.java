8c973e290471f599bc15e0aba5c6a9368d314864
tamil <kunju@gmail.com>
2024-04-08 08:14:20 +0530

    j1

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 //your first java program
 class Helloworld{
     public static void main(String[] args) {
-        System.out.println("Hello World!!! program da dei ");
+        System.out.println("Hello World!!! program da dei mandaya ");
     }
 }
 
